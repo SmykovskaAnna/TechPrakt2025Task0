bdb89127fd952746c9901904ebfee70fe5b6f356
zipz231_sav <zipz231_sav@student.ztu.edu.ua>
2025-01-26 21:23:24 +0100

    Додано C# консольний проєкт, який рахує суму двох чисел та виводить її на екран.

---
+++ Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть перше число: ");
+            string input1 = Console.ReadLine();
+            Console.Write("Введіть друге число: ");
+            string input2 = Console.ReadLine();
+
+            if (double.TryParse(input1, out double number1) && double.TryParse(input2, out double number2))
+            {
+                double sum = number1 + number2;
+                Console.WriteLine($"{number1} + {number2} = {sum}");
+            }
+            else
+            {
+                Console.WriteLine("Помилка: введіть коректні числа.");
+            }
+        }
+    }
+}
